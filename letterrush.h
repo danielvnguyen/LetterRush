e9d8e7fdc5920cf10b664fce3e994856550b9a15
Daniel Nguyen <danielnguyen604@gmail.com>
2021-08-24 16:10:18 -0700

    Update letterrush.h

--- letterrush.h
+++ letterrush.h
@@ -1,1 +1,61 @@
 //Function Definitions:
+
+//Will probably need more libraries for timer, etc.
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//hashTable class to hold all of the valid words/strings:
+class hashTable
+{
+    private:
+    //dynamic string array to hold the dictionary
+    string* stringArray;
+    int arraySize;
+    int currentSize;
+    /*
+    we are using double hashing method for open
+    addressing and dealing with collisions. this
+    value will be used in the 2nd hash function.
+    */
+    int hashTwoValue;
+
+    //Helper methods that shouldn't be visible will go here:
+    int findGreaterPrime(int value);
+
+    bool isPrime(int value);
+
+    int hornerMethod(string inputString, int value);
+
+    int hashFunctionOne(string inputString);
+
+    int hashFunctionTwo(string inputString);
+
+    public:
+    
+    hashTable();
+
+    hashTable(int size);
+
+    ~hashTable();
+
+    void insert(string inputString);
+
+    bool find(string inputString);
+
+    int size();
+
+    int maxSize();
+
+    double loadFactor();
+
+};
+
+/*
+- Functions regarding dictionary, printing, and result vector here:
+
+*/
