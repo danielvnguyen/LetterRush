8a4661d51e3702a7953926b2bf0fcf25b51dd775
Daniel Nguyen <danielnguyen604@gmail.com>
2021-08-25 21:23:07 -0700

    Update mainfile.cpp

--- mainfile.cpp
+++ mainfile.cpp
@@ -3,6 +3,18 @@
 int main()
 {
     hashTable dictTable;
+    string filename = "dictionary.txt", word;
+    ifstream file(filename);
+
+    if (file.is_open())
+    {
+        while (file >> word)
+        {
+            dictTable.insert(word);
+        }
+    }
+    file.close();
+
 
     return 0;
 }
